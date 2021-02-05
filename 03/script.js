560f2f6db3c541b6f8f92e21212e9450c776ef4d
haschish <haschish@gmail.com>
2021-02-05 13:34:50 +0500

    init

---
+++ 03/script.js
@@ -0,0 +1,9 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+ctx.fillRect(0, 0, 100, 100);
+
+
+console.log(canvas.offsetWidth, canvas.offsetHeight)
+
+
